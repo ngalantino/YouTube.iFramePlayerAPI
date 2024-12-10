5d93026b8f274372dfdb7df1ce1a2472bada39d7
Nicolas Galantino <nick.galantino@gmail.com>
2024-12-09 19:15:08 -0500

    Buttons and event handler for switching video in iFrame.

--- example.js
+++ example.js
@@ -1,19 +1,19 @@
       // 2. This code loads the IFrame Player API code asynchronously.
-      var tag = document.createElement('script');
+      let tag = document.createElement('script');
 
       tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
+      let firstScriptTag = document.getElementsByTagName('script')[0];
       firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
       // 3. This function creates an <iframe> (and YouTube player)
       //    after the API code downloads.
-      var player;
+      let player;
       function onYouTubeIframeAPIReady() {
         player = new YT.Player('player', {
           height: '390',
           width: '640',
           videoId: 'M7lc1UVf-VE',
-          playerVars: {
+          playerlets: {
             'playsinline': 1
           },
           events: {
@@ -31,7 +31,7 @@
       // 5. The API calls this function when the player's state changes.
       //    The function indicates that when playing a video (state=1),
       //    the player should play for six seconds and then stop.
-      var done = false;
+      let done = false;
       function onPlayerStateChange(event) {
         if (event.data == YT.PlayerState.PLAYING && !done) {
           setTimeout(stopVideo, 6000);
@@ -40,4 +40,10 @@
       }
       function stopVideo() {
         player.stopVideo();
+      }
+
+      
+      // Event handler to load alternate videos into iFrame.
+      function selectAlternateVideo(id) {
+        player.loadVideoById(`${id}`, 5, "large");
       }
